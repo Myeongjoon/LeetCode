bad0c138eeb62047a283c423332445ad64a314f7
Joon <kimmj8409@gmail.com>
2022-07-10 15:49:35 +0900

    Time: 10 ms (39.87%), Space: 43.6 MB (35.43%) - LeetHub

--- 93-restore-ip-addresses/93-restore-ip-addresses.java
+++ 93-restore-ip-addresses/93-restore-ip-addresses.java
@@ -1,44 +1,20 @@
 class Solution {
-  int n;
-  String s;
-  LinkedList<String> segments = new LinkedList<String>();
-  ArrayList<String> output = new ArrayList<String>();
-
-  public boolean valid(String segment) {
-    int m = segment.length();
-    if (m > 3)
-      return false;
-    return (segment.charAt(0) != '0') ? (Integer.valueOf(segment) <= 255) : (m == 1);
-  }
-
-  public void update_output(int curr_pos) {
-    String segment = s.substring(curr_pos + 1, n);
-    if (valid(segment)) {
-      segments.add(segment);
-      output.add(String.join(".", segments));
-      segments.removeLast();
+    public List<String> restoreIpAddresses(String s) {
+        List<String> result = new ArrayList<>();
+        doRestore(result, "", s, 0);
+        return result;
     }
-  }
-
-  public void backtrack(int prev_pos, int dots) {
-    int max_pos = Math.min(n - 1, prev_pos + 4);
-    for (int curr_pos = prev_pos + 1; curr_pos < max_pos; curr_pos++) {
-      String segment = s.substring(prev_pos + 1, curr_pos + 1);
-      if (valid(segment)) {
-        segments.add(segment); 
-        if (dots - 1 == 0)    
-          update_output(curr_pos);  
-        else
-          backtrack(curr_pos, dots - 1);  
-        segments.removeLast(); 
-      }
+    
+    private void doRestore(List<String> result, String path, String s, int k) {
+        if (s.isEmpty() || k == 4) {
+            if (s.isEmpty() && k == 4)
+                result.add(path.substring(1));
+            return;
+        }
+        for (int i = 1; i <= (s.charAt(0) == '0' ? 1 : 3) && i <= s.length(); i++) { // Avoid leading 0
+            String part = s.substring(0, i);
+            if (Integer.valueOf(part) <= 255)
+                doRestore(result, path + "." + part, s.substring(i), k + 1);
+        }
     }
-  }
-
-  public List<String> restoreIpAddresses(String s) {
-    n = s.length();
-    this.s = s;
-    backtrack(-1, 3);
-    return output;
-  }
 }
