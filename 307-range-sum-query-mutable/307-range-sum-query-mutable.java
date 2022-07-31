90a0c6a0281ac07a73d31a5cd4a7d84984d5d260
Joon <kimmj8409@gmail.com>
2022-07-31 12:16:40 +0900

    Time: 200 ms (16.85%), Space: 127.5 MB (67.05%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -1,24 +1,24 @@
 class NumArray {
 
-    int[] tree;
+    int[] segment;
     int n;
     public NumArray(int[] nums) {
         if (nums.length > 0) {
             n = nums.length;
-            tree = new int[n * 2];
+            segment = new int[n * 2];
             buildTree(nums);
         }
     }
     private void buildTree(int[] nums) {
         for (int i = n, j = 0;  i < 2 * n; i++,  j++)
-            tree[i] = nums[j];
+            segment[i] = nums[j];
         for (int i = n - 1; i > 0; --i)
-            tree[i] = tree[i * 2] + tree[i * 2 + 1];
+            segment[i] = segment[i * 2] + segment[i * 2 + 1];
     }
     
     void update(int pos, int val) {
         pos += n;
-        tree[pos] = val;
+        segment[pos] = val;
         while (pos > 0) {
             int left = pos;
             int right = pos;
@@ -27,7 +27,7 @@ class NumArray {
             } else {
                 left = pos - 1;
             }
-            tree[pos / 2] = tree[left] + tree[right];
+            segment[pos / 2] = segment[left] + segment[right];
             pos /= 2;
         }
     }
@@ -38,11 +38,11 @@ class NumArray {
         int sum = 0;
         while (l <= r) {
             if ((l % 2) == 1) {
-               sum += tree[l];
+               sum += segment[l];
                l++;
             }
             if ((r % 2) == 0) {
-               sum += tree[r];
+               sum += segment[r];
                r--;
             }
             l /= 2;
