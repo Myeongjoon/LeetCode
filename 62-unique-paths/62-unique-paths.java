6a59b9984e0780a53224e87ff5fdcb7ad205d1c3
Joon <kimmj8409@gmail.com>
2022-08-01 10:01:22 +0900

    Time: 0 ms (100.00%), Space: 39.3 MB (89.94%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,15 +1,17 @@
 class Solution {
   public int uniquePaths(int m, int n) {
-    int[][] d = new int[m][n];
+    int[][] dp = new int[m][n];
 
-    for(int[] arr : d) {
-      Arrays.fill(arr, 1);
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            dp[i][j] = 1;
+        }
     }
     for(int col = 1; col < m; ++col) {
       for(int row = 1; row < n; ++row) {
-        d[col][row] = d[col - 1][row] + d[col][row - 1];
+        dp[col][row] = dp[col - 1][row] + dp[col][row - 1];
       }
     }
-    return d[m - 1][n - 1];
+    return dp[m - 1][n - 1];
   }
 }
