b3c3e7f751ec8a44ccd9cbe9c84c940d1f080054
Joon <kimmj8409@gmail.com>
2024-09-18 20:01:37 +0900

    Time: 11 ms (28.3%), Space: 49.2 MB (59.64%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -2,39 +2,33 @@ class NumArray {
 
     public static int[] tree;
     public static int[] nums;
-
     public NumArray(int[] nums) {
         int count = nums.length;
-        tree = new int[count + 1]; // 1-based index, so tree size is count + 1
-        NumArray.nums = nums;
-        
-        // Initialize the tree by updating the tree with nums values
-        for (int i = 0; i < count; i++) {
-            makeNode(i + 1, nums[i]); // 1-based index for tree, 0-based for nums
+        tree = new int[count+1];
+                NumArray.nums = nums;
+        for(int i=1;i<=count;i++){
+            makeNode(i, nums[i-1]);
         }
     }
 
-    // Updates the Fenwick Tree with the value `target` at index `i`
-    public void makeNode(int i, int target) {
-        while (i < tree.length) {
-            tree[i] += target;
-            i += i & -i;  // Move to the next index
+    public void makeNode(int i, int target){
+        while(i<tree.length){
+          tree[i] += target;
+          i += i& -i;  
         }
     }
 
-    // Get the prefix sum up to index `i`
-    public int sum(int i) {
+    public int sum(int i){
         int response = 0;
-        while (i > 0) {
+        while(i >0){
             response += tree[i];
-            i -= i & -i; // Move to the parent index
+            i = i - (i& -i);
         }
         return response;
     }
     
-    // Get the sum of the range [left, right] (0-based index)
     public int sumRange(int left, int right) {
-        return sum(right + 1) - sum(left); // sum(left - 1) for prefix sum
+        return sum(right+1) - sum(left);
     }
 }
 
