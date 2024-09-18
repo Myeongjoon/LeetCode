34b8478c527f1eb8b74505cc17eb5adb7029aef5
Joon <kimmj8409@gmail.com>
2024-09-18 20:58:49 +0900

    Time: 70 ms (96.38%), Space: 75.3 MB (74.12%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
@@ -1,49 +1,41 @@
-class NumArray {
-    static int[] trees;
-    static int size;
+
+
+ class NumArray {
+
+    public static int[] tree;
+    public static int[] nums;
     public NumArray(int[] nums) {
-        size = nums.length;
-        trees = new int[size*3];
-        for(int i=0;i<size;i++){
-            trees[i + size] = nums[i];
+        int count = nums.length;
+        tree = new int[count+1];
+                NumArray.nums = nums;
+        for(int i=1;i<=count;i++){
+            makeNode(i, nums[i-1]);
         }
-        for(int i= size - 1;i>0;i--){
-            trees[i] = trees[i * 2] + trees[i * 2 + 1];
+    }
+
+    public void makeNode(int i, int target){
+        while(i<tree.length){
+          tree[i] += target;
+          i += i& -i;  
         }
     }
     
-    public void update(int index, int val) {
-        index += size;
-        trees[index] = val;
-        while(index > 1){
-            index /= 2;
-            trees[index] = trees[2*index] + trees[2*index + 1];
-        }
+    public void update(int i, int val) {
+        int diff = val - nums[i];
+        nums[i] = val;
+        makeNode(i+1, diff);
     }
-    
-    public int sumRange(int left, int right) {
-        left += size;
-        right += size + 1;  // [left, right] 구간을 포함하도록 수정
+
+    public int sum(int i){
         int response = 0;
-        while(left < right){
-            if(left % 2 == 1){
-                response += trees[left];
-                left ++;
-            }
-            if(right % 2 == 1){
-                right --;
-                response += trees[right];
-            }
-            left /= 2;
-            right /= 2;
+        while(i >0){
+            response += tree[i];
+            i = i - (i& -i);
         }
         return response;
     }
+    
+    public int sumRange(int left, int right) {
+        return sum(right+1) - sum(left);
+    }
 }
-
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray obj = new NumArray(nums);
- * obj.update(index,val);
- * int param_2 = obj.sumRange(left,right);
- */
